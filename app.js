053dd393a60e435a9414c5852194c6f2c3a8ac12
yasingultekin <yasingultekin13@gmail.com>
2022-06-25 14:32:40 +0300

    js

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const text = "aaa";
-const vowels = ["a", "e", "i", "ı", "o", "ö", "u", "ü"];
+// const text = "aaa";
+// const vowels = ["a", "e", "i", "ı", "o", "ö", "u", "ü"];
 
-const textArray = text.split("");
+// const textArray = text.split("");
 
-console.log(textArray);
+// console.log(textArray);
 
-// ? const result = textArray.filter((x) => vowels.indexOf(x) !== -1).length;
+// // ? const result = textArray.filter((x) => vowels.indexOf(x) !== -1).length;
 
-const result = textArray.filter((x) => vowels.includes(x)).length;
+// const result = textArray.filter((x) => vowels.includes(x)).length;
 
-console.log(result);
+// console.log(result);
 
 const input = document.querySelector("input");
 
